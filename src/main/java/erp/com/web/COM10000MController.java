87586a2e1dda9eec318d917eff2c9271c0787943
nbdf2026 <nbdf2026@gmail.com>
2026-06-12 17:31:13 +0900

    nbdf 프로젝트  - 작업일자 : 2026.06.12  - 작업내역 : 공통코드 조회

--- src/main/java/erp/com/web/COM10000MController.java
+++ src/main/java/erp/com/web/COM10000MController.java
@@ -5,9 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.nexacro.uiadapter.spring.core.data.NexacroResult;
+
 @Controller
 @RequestMapping(value = "com/COM10000M")
 public class COM10000MController {
 	
 	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	public NexacroResult selectCodeList() {
+		
+		log.debug("############################################################");
+		
+		return null; 
+	}
 }
