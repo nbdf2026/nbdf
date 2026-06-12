6309cceba6af92408d38831095e110f28bb6aa6e
nbdf2026 <nbdf2026@gmail.com>
2026-06-12 17:24:45 +0900

    nbdf 프로젝트  - 작업일자 : 2026.06.12  - 전체 프로그램 commit and push

---
+++ src/main/java/erp/com/web/COM10000MController.java
@@ -0,0 +1,13 @@
+package erp.com.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "com/COM10000M")
+public class COM10000MController {
+	
+	private Logger log = LoggerFactory.getLogger(getClass());
+}
