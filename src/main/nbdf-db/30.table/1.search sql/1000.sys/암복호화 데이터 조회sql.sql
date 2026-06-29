20cf01c39bcf6c00331c568cd0860d20d031b18b
dims1111 <dims1111@gmail.com>
2026-06-29 15:13:42 +0900

    [nbdf 프로젝트]  1. 작업일자 : 2026.06.29  2. 작업내용   - DB 암복호화 함수 생성     : encrypt_f, decrypt_f, encrypt_password_f

---
+++ src/main/nbdf-db/30.table/1.search sql/1000.sys/암복호화 데이터 조회sql.sql
@@ -0,0 +1,11 @@
+﻿-- 암호화
+select encrypt_f('강정기')
+  from dual;
+
+-- 복호화  
+select decrypt_f('0DF254A42E4D134BA1EFDA85C13827BB')
+  from dual;
+
+-- 비밀번호 암호화  
+select encrypt_password_f('kjgKKK0527!@')
+  from dual;
